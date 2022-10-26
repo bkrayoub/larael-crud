53ef399c22e85270b6aa06644e6453291914fb39
BkrAyoub <92027292+bkrayoub@users.noreply.github.com>
2022-10-26 10:38:17 +0100

    Update index_search.blade.php

--- resources/views/index_search.blade.php
+++ resources/views/index_search.blade.php
@@ -6,14 +6,14 @@
         <th>Actoin</th>
     </tr>
     @foreach ($data as $row)
-    <tr>
-        <td>{{$row->id}}</td>
-        <td>{{$row->name}}</td>
-        <td>
-            <a href="edit/{{$row->id}}">Edit</a>
-            <a href="delete/{{$row->id}}" style="color: darkred">Delete</a>
-        </td>
-    </tr>
+        <tr>
+            <td>{{$row->id}}</td>
+            <td>{{$row->name}}</td>
+            <td>
+                <a href="edit/{{$row->id}}">Edit</a>
+                <a href="delete/{{$row->id}}" style="color: darkred">Delete</a>
+            </td>
+        </tr>
     @endforeach
 
 </table>
